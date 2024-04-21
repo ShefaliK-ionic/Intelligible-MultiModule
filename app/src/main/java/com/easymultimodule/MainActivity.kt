db00f5a1a552db98cac0262da4c9dfc821add333
Shefali Kanojiya <shefalismt@gmail.com>
2024-04-21 18:23:06 +0530

    easy multi module update

--- app/src/main/java/com/easymultimodule/MainActivity.kt
+++ app/src/main/java/com/easymultimodule/MainActivity.kt
@@ -2,13 +2,34 @@ package com.easymultimodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.easymultimodule.databinding.ActivityMainBinding
 import com.sharedpref.BasicActivity
 import com.sharedpref.showToast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        showToast("Meri mumma sabse pyari")
+
+        activityMainBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        clickEvent()
+
+
+
+
+    }
+
+    private fun clickEvent() {
+        activityMainBinding.btnLogin.setOnClickListener {
+            if(activityMainBinding.etUsername.text.toString().isBlank()){
+                showToast("Please enter username")
+            }else if(activityMainBinding.etEmail.text.toString().isBlank()){
+                showToast("Please enter email")
+            }else{
+             showToast("Login success" )
+            }
+        }
     }
 }
