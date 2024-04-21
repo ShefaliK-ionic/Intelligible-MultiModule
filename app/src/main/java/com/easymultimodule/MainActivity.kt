070151c6afdfd93e1b29f28d66e9f2693ccafed8
Shefali Kanojiya <shefalismt@gmail.com>
2024-04-21 17:26:00 +0530

    toast module

--- app/src/main/java/com/easymultimodule/MainActivity.kt
+++ app/src/main/java/com/easymultimodule/MainActivity.kt
@@ -2,10 +2,13 @@ package com.easymultimodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sharedpref.BasicActivity
+import com.sharedpref.showToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showToast("Meri mumma sabse pyari")
     }
 }
---
+++ shared_pref/src/main/java/com/sharedpref/BasicActivity.kt
@@ -0,0 +1,18 @@
+package com.sharedpref
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class BasicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+
+//        showToast()
+
+    }
+
+
+
+}
